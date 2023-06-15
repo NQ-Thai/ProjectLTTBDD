a850f000da60d8b9fcd358fe82954069aa10a22e
NQ-Thai <thainq200618.cdsg@saigontech.edu.vn>
2023-06-15 14:07:17 +0700

    thêm, xóa sản phẩm trong giỏ hàng, tính tổng giá

--- project/project/lib/services/firestore_services.dart
+++ project/project/lib/services/firestore_services.dart
@@ -1,14 +1,32 @@
 import 'package:project/consts/consts.dart';
 
 class FirestorServices {
-
   //get user data
   static getUser(uid) {
-    return fireStore.collection(usersCollection).where('id', isEqualTo: uid).snapshots();
+    return fireStore
+        .collection(usersCollection)
+        .where('id', isEqualTo: uid)
+        .snapshots();
   }
 
   //get products to category
   static getProducts(category) {
-    return fireStore.collection(productsCollection).where('p_category', isEqualTo: category).snapshots();
+    return fireStore
+        .collection(productsCollection)
+        .where('p_category', isEqualTo: category)
+        .snapshots();
+  }
+
+  //get cart
+  static getCart(uid) {
+    return fireStore
+        .collection(cartCollection)
+        .where('added_by', isEqualTo: uid)
+        .snapshots();
+  }
+
+  //delete document
+  static deleteDocument(docId) {
+    return fireStore.collection(cartCollection).doc(docId).delete();
   }
 }
